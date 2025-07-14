6f04b7b5f89ac8aa458accd961b4ee226d652131
huynam-dana <nam.trinh@danaexperts.io.vn>
2025-07-14 17:16:24 +0700

    chore: lint code

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,51 @@
-import { useEffect, useState } from 'react'
-import PostHeader from './modules/posts/components/post-header'
-import PostFilterBox from './modules/posts/components/post-filter-box'
-import PostList from './modules/posts/components/post-list'
-import api from './services/api'
-import type { Post, PostFilter } from './types/post'
+import { useEffect, useState } from "react";
+import PostHeader from "./modules/posts/components/post-header";
+import PostFilterBox from "./modules/posts/components/post-filter-box";
+import PostList from "./modules/posts/components/post-list";
+import api from "./services/api";
+import type { Post, PostFilter } from "./types/post";
 
 function App() {
-  const [posts, setPosts] = useState<Post[]>([])
-  const [authors, setAuthors] = useState<string[]>([])
-  const [categories, setCategories] = useState<string[]>([])
+  const [posts, setPosts] = useState<Post[]>([]);
+  const [authors, setAuthors] = useState<string[]>([]);
+  const [categories, setCategories] = useState<string[]>([]);
 
   const [filter, setFilter] = useState<PostFilter>({
-    search: '',
-    category: 'all',
-    author: 'all',
-    orderBy: 'Newest First',
-  })
+    search: "",
+    category: "all",
+    author: "all",
+    orderBy: "Newest First",
+  });
 
   const fetchPosts = async () => {
-    const response = await api.get<Post[]>('/tracking-order')
-    console.log(response)
-    setPosts(response.data)
-    setAuthors([...new Set(response.data.map((post: Post) => post.author.name))])
-    setCategories([...new Set(response.data.map((post: Post) => post.categories))])
-  }
+    const response = await api.get<Post[]>("/tracking-order");
+    setPosts(response.data);
+    setAuthors([
+      ...new Set(response.data.map((post: Post) => post.author.name)),
+    ]);
+    setCategories([
+      ...new Set(response.data.map((post: Post) => post.categories)),
+    ]);
+  };
 
   useEffect(() => {
-    fetchPosts()
-  }, [])
+    fetchPosts();
+  }, []);
 
   return (
     <>
-      <div className='container mx-auto px-4 py-8'>
-        <PostHeader authors={authors} categories={categories}/>
-        <PostFilterBox filter={filter} onChangeFilter={setFilter} 
-          authors={authors} categories={categories}
+      <div className="container mx-auto px-4 py-8">
+        <PostHeader authors={authors} categories={categories} />
+        <PostFilterBox
+          filter={filter}
+          onChangeFilter={setFilter}
+          authors={authors}
+          categories={categories}
         />
-        <PostList posts={posts} filter={filter}/>
+        <PostList posts={posts} filter={filter} />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
